abb1ed55b4125638e94edf54637573d2743346a3
tmvf <tmvf@yandex.ru>
2017-05-14 14:21:48 +0400

    add class New

---
+++ src/main/java/rr/tt/New.java
@@ -0,0 +1,30 @@
+package rr.tt;
+
+public class New extends Rt{
+    private String fname;
+    private String lname;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    @Override
+    public String toString() {
+        return "New{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                '}';
+    }
+}
